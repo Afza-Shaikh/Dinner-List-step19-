824b98c040427064e01c0493b1cca4d3b837d2fc
afza shaikh <shaikhafza961@gmail.com>
2024-09-16 22:15:31 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const app_1 = require("../14.Guest_list/app");
+console.log(`\n number of Guest`);
+console.log(`we have sucessfully imported ${app_1.guest_list} from Ex 14.`);
---
+++ app.ts
@@ -0,0 +1,5 @@
+import{ guest_list } from '../14.Guest_list/app';
+
+console.log(`\n number of Guest`);
+console.log(`we have sucessfully imported ${guest_list} from Ex 14.`)
+
